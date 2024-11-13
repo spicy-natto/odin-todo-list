7c6db2084cf99edf16f28d8a58b07b6ea07b9e72
Jeff Leep <jdleep@protonmail.com>
2024-11-13 16:23:54 -0600

    Clean task tests

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -7,19 +7,10 @@ describe("Task ID", () => {
     "HIGH",
     "01-01-2025"
   );
-  
-  test("Should return initial value", () => {
-    expect(task.id).toEqual(1);
-  });
 
-  test("Should throw typeError if you attempt to set it", () => {
-    try {
+  test("Throws typeError if you attempt to set it", () => {
+    expect(() => {
       task.id = "456";
-    } catch (e) {
-      expect(e.name).toEqual("TypeError");
-      expect(e.message).toEqual(
-        "Cannot set property id of #<Task> which has only a getter"
-      );
-    }
+    }).toThrow(TypeError)
   });
 });
