2c8ddebf174c5761611b6e51dd4c326b13b03166
Jeff Leep <jdleep@protonmail.com>
2024-11-14 16:58:04 -0600

    Add equals function to task object

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,19 +1,28 @@
 class Task {
-    static currID = 0;
-    #id;
-    
-    constructor(name, description, priority, dueDate) {
-        Task.currID++;
-        this.#id = Task.currID;
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
-        this.dueDate = dueDate;
-    }
+  static currID = 0;
+  #id;
 
-    get id() {
-        return this.#id;
-    }
+  constructor(name, description, priority, dueDate) {
+    Task.currID++;
+    this.#id = Task.currID;
+    this.name = name;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  equals(task) {
+    return (
+      this.name === task.name &&
+      this.description === task.description &&
+      this.priority === task.priority &&
+      this.dueDate === task.dueDate
+    );
+  }
 }
 
 export default Task;
--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -1,16 +1,41 @@
 import Task from "../src/modules/task";
-
-describe("Task ID", () => {
-  const task = new Task(
+function initializeTask() {
+  return new Task(
     "test task",
     "test description",
     "HIGH",
     "01-01-2025"
   );
+}
 
+let task;
+
+beforeEach(() => {
+  task = initializeTask();
+});
+
+describe("ID", () => {
   test("Throws typeError if you attempt to set it", () => {
     expect(() => {
       task.id = "456";
     }).toThrow(TypeError)
   });
 });
+
+describe("equals", () => {
+  
+  test("is true", () => {
+    let newTask = initializeTask();
+    expect(task.equals(newTask)).toBe(true);
+  });
+
+  test("is false", () => {
+    let newTask = new Task(
+      "not equals task",
+      "test description",
+      "HIGH",
+      "01-01-2025"
+    );  
+    expect(task.equals(newTask)).toBe(false);
+  });
+})
