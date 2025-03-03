eb379e2acb986d3f0c11a9eb50aaf908331e2308
Jeff Leep <jdleep@protonmail.com>
2025-03-03 16:23:39 -0600

    Add htmlToNode function to use in view

--- src/modules/utilities/utilities.js
+++ src/modules/utilities/utilities.js
@@ -21,9 +21,28 @@ function itemArrayToObject(itemArray) {
   }, {});
 }
 
+// Taken from stack overflow 
+// https://stackoverflow.com/a/35385518
+
+function htmlToNode(html) {
+  const template = document.createElement('template');
+  template.innerHTML = html.trim();
+  const nNodes = template.content.childNodes.length;
+  if (nNodes !== 1) {
+      throw new Error(
+          `html parameter must represent a single node; got ${nNodes}. ` +
+          'Note that leading or trailing spaces around an element in your ' +
+          'HTML, like " <img/> ", get parsed as text nodes neighbouring ' +
+          'the element; call .trim() on your input to avoid this.'
+      );
+  }
+  return template.content.firstChild;
+}
+
 export default {
   idMatches,
   compareItemsById,
   itemArraysAreEqual,
   itemArrayToObject,
+  htmlToNode,
 };
