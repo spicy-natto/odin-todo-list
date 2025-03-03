e4c69f481b34c579d9951cd50f28fa3e04c83cd3
Jeff Leep <jdleep@protonmail.com>
2025-03-03 13:30:41 -0600

    Build HTML / CSS framework

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,7 @@
-module.exports = merge(common, {
+import common from "./webpack.common.js";
+import { merge } from "webpack-merge";
+
+export default merge(common, {
     mode: 'development',
     devtool: "eval-source-map",
     devServer: {
