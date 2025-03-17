a31ff81f347d53188163e2d441e8fbd9dc045b73
Jeff Leep <jdleep@protonmail.com>
2025-03-17 15:46:44 -0500

    Remove unecessary parameters from Project

--- src/modules/items/Project.js
+++ src/modules/items/Project.js
@@ -3,12 +3,9 @@ import shortUUID from "short-uuid";
 class Project {
     objectType = "project";
 
-    constructor({id, name, description, priority, dueDate}) {
+    constructor({id, name}) {
         this.id = id ? id : shortUUID.generate();
         this.name = name;
-        this.description = description;
-        this.priority = priority;
-        this.dueDate = dueDate;
     }
 
     equals(project) {
