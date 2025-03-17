9f7d97216af251dcc8301ca5003cf70ca1c35047
Jeff Leep <jdleep@protonmail.com>
2025-03-17 15:48:02 -0500

    Make id in SidebarItem public

--- src/modules/items/SidebarItem.js
+++ src/modules/items/SidebarItem.js
@@ -1,19 +1,14 @@
 import shortUUID from "short-uuid";
 
 class SidebarItem {
-    #id;
     itemType = "sideBarItem";
 
     constructor({id, name, filterType = "All", filterData, svg}) {
-        this.#id = id ? id : shortUUID.generate();
+        this.id = id ? id : shortUUID.generate();
         this.svg = svg;
         this.name = name;
         this.filterType = filterType;
         this.filterData = filterData;
-    }
-
-    get id() {
-        return this.#id;
     }
 }
 
